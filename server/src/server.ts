33594da9095ff4af64321ae75f1b73f698d7a212
Raj Shah <t-rajshah@microsoft.com>
2021-06-09 12:13:35 -0700

    Repro bug

--- server/src/server.ts
+++ server/src/server.ts
@@ -54,7 +54,8 @@ connection.onInitialize((params: InitializeParams) => {
 			textDocumentSync: TextDocumentSyncKind.Incremental,
 			// Tell the client that this server supports code completion.
 			completionProvider: {
-				resolveProvider: true
+				resolveProvider: true,
+				triggerCharacters: [':', '"']
 			}
 		}
 	};
